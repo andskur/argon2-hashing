cd524c9aa10ce19a51ead04a198aba29c3850f59
Andrew Skurlatov <a.skurlatov@gmail.com>
2019-01-05 00:45:06 +0300

    [TESTS] Added tests dor common functions

---
+++ argon2_test.go
@@ -0,0 +1,63 @@
+package argon2
+
+import (
+	"testing"
+)
+
+var (
+	testLengths = []uint32{1, 8, 16, 32, 128, 512, 2048}
+	password    = "qwerty123"
+)
+
+var testParams = []struct {
+	pass   bool
+	params *Params
+}{
+	{true, &Params{Memory: 8 * 1024, Iterations: 1, Parallelism: 1, SaltLength: 8, KeyLength: 16}}, // minimum values
+	{true, &Params{Memory: 32 * 1024, Iterations: 2, Parallelism: 2, SaltLength: 16, KeyLength: 32}},
+	{true, &Params{Memory: 64 * 1024, Iterations: 3, Parallelism: 4, SaltLength: 32, KeyLength: 64}},
+	{true, &Params{Memory: 256 * 1024, Iterations: 4, Parallelism: 8, SaltLength: 64, KeyLength: 128}},
+	{false, &Params{Memory: 4 * 1024, Iterations: 3, Parallelism: 2, SaltLength: 16, KeyLength: 32}},  // invalid Memory
+	{false, &Params{Memory: 64 * 1024, Iterations: 0, Parallelism: 2, SaltLength: 16, KeyLength: 32}}, // invalid Iterations
+	{false, &Params{Memory: 64 * 1024, Iterations: 3, Parallelism: 0, SaltLength: 16, KeyLength: 32}}, // invalid Parallelism
+	{false, &Params{Memory: 64 * 1024, Iterations: 3, Parallelism: 2, SaltLength: 4, KeyLength: 32}},  // invalid SaltLength
+	{false, &Params{Memory: 64 * 1024, Iterations: 3, Parallelism: 2, SaltLength: 16, KeyLength: 8}},  // invalid KeyLength
+}
+
+func TestGenerateRandomBytes(t *testing.T) {
+	for _, v := range testLengths {
+		_, err := GenerateRandomBytes(v)
+		if err != nil {
+			t.Fatal("failed to generate random bytes")
+		}
+	}
+}
+
+func TestGenerateFromPassword(t *testing.T) {
+	for _, v := range testParams {
+		_, err := GenerateFromPassword([]byte(password), v.params)
+		if err != nil && v.pass == true {
+			t.Fatalf("no error was returned when expected for params: %+v", v.params)
+		}
+	}
+}
+
+func TestCompareHashAndPassword(t *testing.T) {
+	hash, err := GenerateFromPassword([]byte(password), DefaultParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := CompareHashAndPassword(hash, []byte(password)); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := CompareHashAndPassword(hash, []byte("invalid-password")); err == nil {
+		t.Fatalf("mismatched passwords did not produce an error")
+	}
+
+	invalidHash := []byte("argon2id$19$65536$3$4$J3XY52LfC3pgj4gsqy646g$FQb/pMqSDAwZ51NfShsVsPKvLapOltJBlLlq3Qg3wX8")
+	if err := CompareHashAndPassword(invalidHash, []byte(password)); err == nil {
+		t.Fatalf("did not identify an invalid hash")
+	}
+}
